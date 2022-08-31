2c5e0e6f0d78caa9132738f054fd99e4d90325a5
MPLekter <65944671+MPLekter@users.noreply.github.com>
2022-08-31 18:09:52 +0200

    Initial commit - ads working in editor

--- AdManager.cs
+++ AdManager.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 
 //this AdManager is a singleton. 
 //it will exist in every scene. 
 
-public class AdManager : MonoBehaviour
+public class AdManager : MonoBehaviour, IUnityAdsListener
 {
+    [SerializeField] private bool testMode = true;
     public static AdManager Instance;
+
+    private GameOverHandler gameOverHandler;
+#if UNITY_ANDROID
+    private string gameId = "4908965";
+#endif
+#if UNITY_IOS
+    private string gameId = "4908964"
+#endif
 
     private void Awake()
     {
@@ -23,7 +33,48 @@ public class AdManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+
+            Advertisement.AddListener(this); 
+            Advertisement.Initialize(gameId, testMode);
+
         }
     }
 
+    public void ShowAd(GameOverHandler gameOverHandler)
+    {
+        Debug.Log($"AdManager here called by {gameOverHandler.name}");
+        this.gameOverHandler = gameOverHandler; //a passed gameOver handler from GameOver object/script becomes the cached gameOver here.
+        Advertisement.Show("ContinueAfterCrash"); //you can copy this from your dashboard.unity3d.com
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        switch (showResult)
+        {
+            case ShowResult.Finished:
+                gameOverHandler.ContinueGame();
+                break;
+            case ShowResult.Skipped:
+                break;
+            case ShowResult.Failed:
+                Debug.LogWarning("Ad Failed");
+                break;
+        }
+    }
+    public void OnUnityAdsReady(string placementId)
+    {
+       Debug.Log($"UnityAdsReady {placementId}");
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        Debug.LogError($"UnityAdsError {message}");
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        Debug.Log($"UnityAdsStarted {placementId}");
+    }
+
+    
 }
